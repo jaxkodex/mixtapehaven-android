34e5aff8c42db2e8ada6a7c0cff673ebb0c8ffe5
jaxkodex <jkodex@gmail.com>
2026-02-09 15:05:06 +0100

    fix: build

--- app/src/main/java/pe/net/libre/mixtapehaven/ui/components/SongContextMenuBottomSheet.kt
+++ app/src/main/java/pe/net/libre/mixtapehaven/ui/components/SongContextMenuBottomSheet.kt
@@ -1,6 +1,7 @@
 package pe.net.libre.mixtapehaven.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -123,14 +124,10 @@ fun SongContextMenuBottomSheet(
                         tint = GunmetalGray
                     )
                 },
-                modifier = Modifier.clickableSafe {
+                modifier = Modifier.clickable {
                     onAddToPlaylist(song)
                 }
             )
         }
     }
-}
-
-private fun Modifier.clickableSafe(onClick: () -> Unit): Modifier {
-    return androidx.compose.foundation.clickable(onClick = onClick)
 }
